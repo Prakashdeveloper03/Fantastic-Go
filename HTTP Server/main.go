4eee02f7e85ce3243123e6017e604e4eee3d0f5f
Siva Prakash <prakashdeveloper@outlook.in>
2024-04-18 12:40:20 +0530

    Updated HTTP Server folder

--- HTTP Server/main.go
+++ HTTP Server/main.go
@@ -31,6 +31,47 @@ func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
+func UpdateUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var updatedUser User
+	err := json.NewDecoder(r.Body).Decode(&updatedUser)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error decoding request body: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	_, ok := users[id]
+	if !ok {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	users[id] = updatedUser
+	w.WriteHeader(http.StatusOK)
+}
+
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	_, ok := users[id]
+	if !ok {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	delete(users, id)
+	w.WriteHeader(http.StatusOK)
+}
+
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -49,7 +90,9 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/user/create", CreateUserHandler)
-
+	http.HandleFunc("/user/get", GetUserHandler)
+	http.HandleFunc("/user/update", UpdateUserHandler)
+	http.HandleFunc("/user/delete", DeleteUserHandler)
 	fmt.Println("Starting server at port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Printf("Error starting server: %v\n", err)
